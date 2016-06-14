ad0c7793f178cad030227e209f123a1c5b7bb418
Cliff Chao-kuan Lu <clifflu@gmail.com>
2016-06-14 10:47:40 +0800

    refactor: argument name to `get_region_shortname`

--- awscatalyst/aws.py
+++ awscatalyst/aws.py
@@ -23,7 +23,7 @@ class Aws(object):
             cls.get_region_magic_url()
 
     @classmethod
-    def get_region_shortname(cls, s=None):
+    def get_region_shortname(cls, region_name=None):
         alternatives = {
             'use1': {'us-east-1', },
             'usw1': {'us-west-1', },
@@ -37,10 +37,10 @@ class Aws(object):
             'euc1': {'eu-central-1', },
         }
 
-        s = s.lower() if s else cls.get_region()
+        region_name = region_name.lower() if region_name else cls.get_region()
 
-        for sn, candidates in alternatives.items():
-            if s in candidates:
-                return sn
+        for shortname, candidates in alternatives.items():
+            if region_name in candidates:
+                return shortname
 
-        return s
+        return region_name
